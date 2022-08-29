52c262dec07ce1954e1c494413f0d104bdda3db1
jianggf <jianggf@seaway.net.cn>
2022-08-29 16:00:16 +0800

    vue初始化

--- examples/vue.js
+++ examples/vue.js
@@ -1,4 +1,34 @@
 // mini-vue
-function Vue(){
-  console.log('vue 构造了');
+function Vue(options){
+  this._init(options);
+}
+
+Vue.prototype._init=function(options){
+  // options
+  this.$options=options;
+  // member init
+  this.$parent=options.parent;
+  // beforeCreated
+  callHook(this,'beforeCreated');
+  // state init
+  // ...
+  // created
+  callHook(this,'created');
+}
+
+function callHook(vm,hook){
+  const hooks=vm.$options[hook];
+  if(hooks){
+    hooks.call(vm);
+  }
+}
+
+Vue.prototype.$mount=function(el){
+  // parent
+  const parent=document.querySelector(el);
+  // data
+  const data=this.$options.data();
+  // append
+  const node=this.$options.render.call(data);
+  parent.appendChild(node);
 }
