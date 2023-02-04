f9d98fabb950eed0a26135068ce7eb7c6fedd90f
lingfengjgf <lingfengjgf@163.com>
2023-02-04 17:00:35 +0800

    编译器实现02--parseText、parseInterpolation

--- examples/compiler.js
+++ examples/compiler.js
@@ -78,6 +78,8 @@ function parseTag(context, isEndTag = false){
   context.advance(match[0].length);
   const isUnary = context.source.startsWith("/>");
   context.advance(isUnary ? 2 : 1);
+
+  // 构造元素节点并返回
   return {
     type:"Element",
     tag,
@@ -86,4 +88,54 @@ function parseTag(context, isEndTag = false){
   }
 }
 
-console.log(JSON.stringify(parse('<div><span></span></div>'),false, ' '))
+function parseText(context){
+  // content:{{text}}</div>
+  let endIdx = context.source.length;
+  const ltIdx = context.source.indexOf('<');
+  const delimiterIdx = context.source.indexOf('{{');
+  if(ltIdx>-1 && ltIdx<endIdx){
+    endIdx = ltIdx;
+  }
+  if(delimiterIdx>-1 && delimiterIdx<endIdx){
+    endIdx = delimiterIdx;
+  }
+
+  // 截取
+  const content = context.source.slice(0,endIdx);
+  context.advance(content.length);
+
+  // 构造文本节点并返回
+  return {
+    type:'Text',
+    content
+  }
+}
+
+function parseInterpolation(context){
+  // 去掉{{
+  context.advance(2);
+
+  // 查找}}
+  const closeIdx = context.source.indexOf('}}');
+
+  if(closeIdx === -1){
+    console.error('插值表达式缺少结束分隔符');
+    return ;
+  }
+
+  const content = context.source.slice(0, closeIdx);
+  // 去掉content的长度
+  context.advance(content.length);
+  // 去掉}}
+    context.advance(2);
+
+  return {
+    type:'Interpolation',
+    content:{
+      type:'Expression',
+      content
+    }
+  }
+}
+
+console.log(JSON.stringify(parse('<div><span>num:{{num}}</span></div>'),false, ' '))
